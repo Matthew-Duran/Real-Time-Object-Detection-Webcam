15f78cead5a5d6c92bb1f83c78394b1f9bf96bc7
Matthew Duran <Matthew.q.duran@gmail.com>
2025-09-04 15:04:30 -0400

    Functional Updates

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+from Webcam.YoloWebcam import run_yolo_webcam
 
+
+def main():
+    print("Starting YOLO webcam...")
+    run_yolo_webcam()
+
+
+if __name__ == "__main__":
+    main()
--- Webcam/YoloWebcam.py
+++ Webcam/YoloWebcam.py
@@ -3,46 +3,47 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4,720)
 
-model = YOLO("../Yolo-Weights/yolov8n")
+def run_yolo_webcam():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+    model = YOLO("../Yolo-Weights/yolov8n")
 
-while True:
-    success, img = cap.read()
-    results = model(img,stream=True)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                  "teddy bear", "hair drier", "toothbrush"]
 
-            #Bounding Box
-            x1,y1,x2,y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    while True:
+        success, img = cap.read()
+        if not success:
+            print("Failed to grab frame")
+            break
 
-            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1,y1,w,h))
+        results = model(img, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                w, h = x2 - x1, y2 - y1
+                cvzone.cornerRect(img, (x1, y1, w, h))
+                conf = math.ceil((box.conf[0]*100)) / 100
+                cls = int(box.cls[0])
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(
+                    0, x1), max(40, y1)), scale=2, thickness=1)
 
-            #Confidence
-            conf = math.ceil((box.conf[0]*100)) / 100
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            #Class Name
-            cls  = int(box.cls[0])
-
-            cvzone.putTextRect(img, f'{classNames[cls]} {conf}',(max(0, x1), max(40, y1)), scale = 2, thickness=1)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
